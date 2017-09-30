d09784c94a1dc4af2b1fdbf6ae195fb143bd257b
Eric Zavesky <ezavesky@research.att.com>
2017-09-30 09:24:08 -0500

    - add balloon game section

---
+++ Assets/UdacityVR/Scripts/BalloonBehavior.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonBehavior : MonoBehaviour {
+	private float timeComplete;
+	private float timeLast;
+	private Vector3 positionComplete;
+	private Vector3 positionInit;
+	private Vector3 positionLast;
+
+	public bool isFree = false;
+	public bool isMagic = false;
+	public float MAX_DRIFT = 1.0f;
+	public float MAX_TIME_DRIFT = 3f;
+
+	// Use this for initialization
+	void Start () {
+		positionLast = positionComplete = positionInit = transform.localPosition;
+		generatePosition ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isFree) {
+			timeLast += Time.deltaTime;
+			if (timeLast >= timeComplete)
+				generatePosition ();
+			transform.localPosition = Vector3.Slerp (positionLast, positionComplete, timeLast / timeComplete);
+		} else {
+			if (transform.localPosition.y > 20) {
+				gameObject.SetActive (false);
+			}
+		}
+	}
+
+	//generate new random target position for balloon
+	private void generatePosition() {
+		timeLast = 0f;
+		positionLast = positionComplete;
+		//random duration for moving around
+		timeComplete = Random.Range(1f, MAX_TIME_DRIFT);
+		//random new target position
+		positionComplete.x = Random.Range (positionLast.x - MAX_DRIFT, positionLast.x + MAX_DRIFT);
+		positionComplete.y = Random.Range (positionLast.y - MAX_DRIFT, positionLast.y + MAX_DRIFT);
+		//bound limit how far away it can go from init position
+		positionComplete.x = Mathf.Min (Mathf.Max (positionComplete.x, positionInit.x - MAX_DRIFT), positionInit.x + MAX_DRIFT);
+		positionComplete.y = Mathf.Min (Mathf.Max (positionComplete.y, positionInit.y - MAX_DRIFT), positionInit.y + MAX_DRIFT);
+	}
+
+	//simulate cutting string or resetting it
+	public void toggleFreezePosition(bool reset=false) {
+		Rigidbody rigidbody = gameObject.GetComponent<Rigidbody> ();
+		//http://answers.unity3d.com/questions/238887/can-you-unfreeze-a-rigidbodyconstraint-position-as.html
+		if ((rigidbody.constraints & RigidbodyConstraints.FreezePositionY)!=0) {
+			rigidbody.constraints &= ~RigidbodyConstraints.FreezePositionY;
+			isFree = true;
+		}
+		if (reset) {
+			rigidbody.constraints |= RigidbodyConstraints.FreezePositionY;
+			isFree = false;
+		}
+	}
+
+	//reset position to start, reattach line
+	public void resetPosition() {
+		gameObject.SetActive (true);
+		transform.localPosition = positionInit;
+		toggleFreezePosition (true);
+	}
+}
---
+++ Assets/UdacityVR/Scripts/GameBalloon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameBalloon : MonoBehaviour {
+	public GameObject containerBalloon;
+	public GameObject gamePrize = null;
+
+
+	//check to see if there was a blue balloon found..
+	public void triggerCheck() {
+		bool magicFound = false;
+		foreach (BalloonBehavior child in transform.GetComponentsInChildren<BalloonBehavior> ()) {
+			//Debug.Log ("CHILD: " + child.isFree + ", magic:" + child.isMagic);
+			if (child.isMagic && !child.isFree) 
+				magicFound = true;
+		}
+		if (!magicFound) {
+			//Debug.Log ("NO MAGIC");
+			if (gamePrize)
+				gamePrize.SetActive (true);
+		}
+	}
+
+	//reset all balloon positions
+	public void triggerReset() {
+		//Debug.Log ("RESET");
+		foreach (BalloonBehavior child in transform.GetComponentsInChildren<BalloonBehavior> ()) {
+			child.resetPosition ();
+		}
+	}
+
+
+}
