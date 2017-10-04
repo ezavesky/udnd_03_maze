bb8af7a1c1f8babd6c6729fdc2f33627333b4ae9
Eric Zavesky <ezavesky@research.att.com>
2017-10-04 05:58:37 -0500

    - comment update for directios to code reviewer

--- Assets/UdacityVR/Scripts/SignPost.cs
+++ Assets/UdacityVR/Scripts/SignPost.cs
@@ -7,6 +7,7 @@ public class SignPost : MonoBehaviour
 	public void ResetScene() 
 	{
         // Reset the scene when the user clicks the sign post
+		// This functionality / script is in the Inventory class instead
 	}
 
 }
