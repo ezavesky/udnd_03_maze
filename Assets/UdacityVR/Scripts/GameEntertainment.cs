8325bfad7db94fbca0b65725a994f0535a5ff4a6
Eric Zavesky <ezavesky@research.att.com>
2017-10-05 11:06:06 -0500

    - add respawn/reset for entertainment game coin

---
+++ Assets/UdacityVR/Scripts/GameEntertainment.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEntertainment : MonoBehaviour, IGameInterface {
+	public GameObject objPrize;
+
+	public void Reset () {
+		if (objPrize)
+			objPrize.SetActive (true);
+	}
+}
